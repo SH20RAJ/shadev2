1d6727bb7c10239660aa83b4081d20f4033a3b58
sh20raj <shaswatraj3@gmail.com>
2024-04-08 23:11:43 +0530

    casc

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const config = {
 
   experimental: {
-    pageDataCollectionTimeout: 1200000, // 2 minutes in milliseconds
+    staticPageGenerationTimeout: 1200000, // 2 minutes in milliseconds
   },
 };
 
