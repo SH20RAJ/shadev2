496269c0aac67af6b5091907d72212cde1f8c79a
sh20raj <shaswatraj3@gmail.com>
2024-04-08 19:30:50 +0530

    all set

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -3,13 +3,15 @@
 const Footer = () => {
   return (
     <>
-      {/* <footer className="bg-gray-800 text-white text-center py-4">
-        <div className="container mx-auto">
-          <p>&copy; 2024 Your Social Media Website. All rights reserved.</p>
-        </div>
-      </footer> */}
-
-      <div dangerouslySetInnerHTML={{ __html: '<script src="/js/sidebar.js"></script>' }}></div>
+    
+    {/* <footer className="bg-gray-800 text-white text-center py-4">
+      <div className="container mx-auto">
+        <p>&copy; 2024 Your Social Media Website. All rights reserved.</p>
+      </div>
+      
+    </footer> */}
+    
+      <div dangerouslySetInnerHTML={{ __html : '<script src="/js/sidebar.js"></script>'}}></div>
     </>
   );
 };
