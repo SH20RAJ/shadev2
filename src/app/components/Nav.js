63541602526729cbaff997563c986e2cbc2b4f0f
Sh Raj <shaswatraj3@gmail.com>
2024-04-08 01:53:26 +0000

    jsx error solved - login working

--- src/app/components/Nav.js
+++ src/app/components/Nav.js
@@ -1,99 +1,99 @@
 // components/Navbar.js
 
 import Link from 'next/link';
-import { getUser } from '../helpers/auth';
+import { getUser, user } from '../helpers/auth';
 
 const Navbar = () => {
   return (
-    <nav class="sidebar close">
+    <nav className="sidebar close">
     <header>
     <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'/>
 
-        <div class="image-text">
-            <span class="image">
-                <img src="/logo.png" className=" rounded-full" alt="logo"/>
+        <div className="image-text">
+            <span className="image">
+                <img src="/logo.png" classNameName=" rounded-full" alt="logo"/>
             </span>
 
-            <div class="text logo-text">
-                <span class="name">Shade</span>
-                <span class="profession">Be Social { (getUser())? "Welcome":"Hii - "+getUser() }</span>
+            <div className="text logo-text">
+                <span className="name">Shade</span>
+                <span className="profession">Be Social { (!user)? "Welcome":"Hii - "+user }</span>
             </div>
         </div>
 
-        <i class='bx bx-chevron-right toggle'></i>
+        <i className='bx bx-chevron-right toggle'></i>
     </header>
 
-    <div class="menu-bar">
-        <div class="menu">
+    <div className="menu-bar">
+        <div className="menu">
 
-            <li class="search-box">
-                <i class='bx bx-search icon'></i>
+            <li className="search-box">
+                <i className='bx bx-search icon'></i>
                 <input type="text" placeholder="Search..."/>
             </li>
 
-            <ul class="menu-links">
-                <li class="nav-link">
+            <ul className="menu-links">
+                <li className="nav-link">
                     <Link href="/">
-                        <i class='bx bx-home-alt icon'></i>
-                        <span class="text nav-text">Home</span>
+                        <i className='bx bx-home-alt icon'></i>
+                        <span className="text nav-text">Home</span>
                     </Link>
                 </li>
 
-                <li class="nav-link">
+                <li className="nav-link">
                     <Link href="/videos">
-                        <i class='bx bx-play-circle icon'></i>
-                        <span class="text nav-text">Videos</span>
+                        <i className='bx bx-play-circle icon'></i>
+                        <span className="text nav-text">Videos</span>
                     </Link>
                 </li>
 
-                <li class="nav-link">
+                <li className="nav-link">
                     <Link href="/notifications">
-                        <i class='bx bx-bell icon'></i>
-                        <span class="text nav-text">Notifications</span>
+                        <i className='bx bx-bell icon'></i>
+                        <span className="text nav-text">Notifications</span>
                     </Link>
                 </li>
 
-                <li class="nav-link">
+                <li className="nav-link">
                     <Link href="/analytics">
-                        <i class='bx bx-pie-chart-alt icon'></i>
-                        <span class="text nav-text">Analytics</span>
+                        <i className='bx bx-pie-chart-alt icon'></i>
+                        <span className="text nav-text">Analytics</span>
                     </Link>
                 </li>
 
-                <li class="nav-link">
+                <li className="nav-link">
                     <Link href="/likes">
-                        <i class='bx bx-heart icon'></i>
-                        <span class="text nav-text">Likes</span>
+                        <i className='bx bx-heart icon'></i>
+                        <span className="text nav-text">Likes</span>
                     </Link>
                 </li>
 
-                <li class="nav-link">
+                <li className="nav-link">
                     <Link href="/account">
-                        <i class='bx bx-user-circle icon'></i>
-                        <span class="text nav-text">Account</span>
+                        <i className='bx bx-user-circle icon'></i>
+                        <span className="text nav-text">Account</span>
                     </Link>
                 </li>
 
             </ul>
         </div>
 
-        <div class="bottom-content">
-            <li class="">
+        <div className="bottom-content">
+            <li className="">
                 <Link href="/logout">
-                    <i class='bx bx-log-out icon'></i>
-                    <span class="text nav-text">Logout</span>
+                    <i className='bx bx-log-out icon'></i>
+                    <span className="text nav-text">Logout</span>
                 </Link>
             </li>
 
-            <li class="mode">
-                <div class="sun-moon">
-                    <i class='bx bx-moon icon moon'></i>
-                    <i class='bx bx-sun icon sun'></i>
+            <li className="mode">
+                <div className="sun-moon">
+                    <i className='bx bx-moon icon moon'></i>
+                    <i className='bx bx-sun icon sun'></i>
                 </div>
-                <span class="mode-text text">Dark mode</span>
+                <span className="mode-text text">Dark mode</span>
 
-                <div class="toggle-switch">
-                    <span class="switch"></span>
+                <div className="toggle-switch">
+                    <span className="switch"></span>
                 </div>
             </li>
 
