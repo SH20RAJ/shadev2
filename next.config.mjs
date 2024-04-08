51255815c2319c7f4eb0cf0fc4bbc0cfab570a44
sh20raj <shaswatraj3@gmail.com>
2024-04-08 21:29:51 +0530

    vercel deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        pageDataCollectionTimeout: 20000, // Increase the timeout (in milliseconds)
+      },
+};
 
 export default nextConfig;
