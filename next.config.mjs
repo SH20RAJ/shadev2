7e486a7753e87a558f8b83426bfaf162b3f28bc7
sh20raj <shaswatraj3@gmail.com>
2024-04-09 17:33:01 +0530

    next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
 
 const config = {
 
-  experimental: {
-    staticPageGenerationTimeout: 1200000, // 2 minutes in milliseconds
-  },
+  staticPageGenerationTimeout: 1200000, // 2 minutes in milliseconds
 };
 
 export default config;
